5010b961249dc8550d1ce2acad8f00f58540052d
Kiru Samapathy <ksama@thoughtworks.com>
2016-05-13 16:49:51 +1000

    Including rspec-wait; Testing if grafana service is running after starting the container

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 gem "rspec"
 gem "serverspec"
-# gem "rspec-wait"
+gem "rspec-wait"
 gem "docker-api"
--- spec/grafana_interface_spec.rb
+++ spec/grafana_interface_spec.rb
@@ -54,57 +54,28 @@ describe "Dockerfile" do
     end
   end
 
-  # describe 'Dockerfile#running' do
-  #   before(:all) do
-  #     #Docker.logger = Logger.new(STDOUT)
-  #     @running_container = Docker::Container.create(
-  #     'Cmd'=>["/go", "start"],
-  #     'Image'=>@image.id,
-  #     'HostConfig' => {
-  #         'PortBindings' => { "#{REMOTE_AGENTS_PORT}/tcp" => [{ 'HostPort' => "#{REMOTE_AGENTS_PORT}" }] }
-  #       },
-  #     'Env'=>[
-  #       'BAMBOO_SERVICE_UID=501',
-  #       'BAMBOO_SERVICE_GID=501'
-  #     ])
-  #     @running_container.start( :detach => true )
-  #     # puts @running_container.json
-  #     set :backend, :docker
-  #     set :os, :family => 'redhat', :release => 7
-  #     set :docker_image, nil
-  #     set :docker_container, @running_container.id
-  #   end
-  #
-  #   after(:all) do
-  #     @running_container.kill
-  #     @running_container.remove
-  #   end
-  #
-  #   describe service('bamboo'), :wait => {:delay =>2, :timeout => 10 } do
-  #     it "should be running" do
-  #       wait_for(subject).to be_running.under('supervisor')
-  #     end
-  #   end
-  #
-  #   describe port("#{BAMBOO_SERVICE_PORT}"), :wait => { :delay => 1, :timeout => 10 } do
-  #     it "should be listening" do
-  #       wait_for(subject).to be_listening
-  #     end
-  #   end
-  #
-  #   describe 'Remote agents port #{REMOTE_AGENTS_PORT}' do
-  #     it "should be exposed" do
-  #       expect(@image.json['ContainerConfig']['ExposedPorts']).to include("#{REMOTE_AGENTS_PORT}/tcp")
-  #     end
-  #   end
-  #
-  #   describe file("/usr/local/bamboo/current/temp") do
-  #     it {should be_writable }
-  #   end
-  #
-  #   describe file('/home/bamboo/') do
-  #     it {should exist}
-  #   end
-  #
-  # end
+  describe 'Dockerfile#running' do
+    before(:all) do
+      #Docker.logger = Logger.new(STDOUT)
+      @running_container = Docker::Container.create(
+      'Image'=>@image.id)
+      @running_container.start( :detach => true )
+      # puts @running_container.json
+      set :backend, :docker
+      set :os, :family => 'redhat', :release => 7
+      set :docker_image, nil
+      set :docker_container, @running_container.id
+    end
+
+    after(:all) do
+      @running_container.kill
+      @running_container.remove
+    end
+
+    describe service('grafana-server'), :wait => {:delay =>2, :timeout => 10 } do
+      it "should be running" do
+        wait_for(subject).to be_running
+      end
+    end
+  end
 end
