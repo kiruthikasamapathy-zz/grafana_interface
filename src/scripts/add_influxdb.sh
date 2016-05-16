fa6904098dce06b8eaaa19c5a7d83bb1e354cf3f
Kiru Samapathy <ksama@thoughtworks.com>
2016-05-16 16:39:20 +1000

    Make everything configurable in influxdb script

--- src/scripts/add_influxdb.sh
+++ src/scripts/add_influxdb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+GRAFANA_USERNAME=${GRAFANA_USERNAME:-admin}
+GRAFANA_PASSWORD=${GRAFANA_PASSWORD:-admin}
+GRAFANA_HOST=${GRAFANA_HOST:-localhost}
+GRAFANA_PORT=${GRAFANA_PORT:-3000}
+INFLUXDB_HOST=${INFLUXDB_HOST:-localhost}
+INFLUXDB_PORT=${INFLUXDB_PORT:-8086}
+
 curl 'http://admin:admin@localhost:3000/api/datasources' \
     -X POST -H "Content-Type: application/json" \
     --data-binary <<DATASOURCE \
