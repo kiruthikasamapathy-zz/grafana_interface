b45f1dbbd0ebee6e86dde7b318b4a8a565aea8de
Kiru Samapathy <ksama@thoughtworks.com>
2016-05-18 14:30:13 +1000

    determine influxdb url based on environment

--- src/scripts/add_influxdb.sh
+++ src/scripts/add_influxdb.sh
@@ -4,16 +4,24 @@ GRAFANA_USERNAME=${GRAFANA_USERNAME:-admin}
 GRAFANA_PASSWORD=${GRAFANA_PASSWORD:-admin}
 GRAFANA_HOST=${GRAFANA_HOST:-localhost}
 GRAFANA_PORT=${GRAFANA_PORT:-3000}
-INFLUXDB_HOST=${INFLUXDB_HOST:-localhost}
+DEPLOY_ENVIRONMENT=${BAMBOO_DEPLOY_ENVIRONMENT:-localhost}
 INFLUXDB_PORT=${INFLUXDB_PORT:-8086}
 
-curl 'http://admin:admin@localhost:3000/api/datasources' \
+if [ "${DEPLOY_ENVIRONMENT}" = "localhost" ]; then
+  INFLUXDB_API="http://localhost:${INFLUXDB_PORT}"
+else
+  INFLUXDB_API="http://server-${DEPLOY_ENVIRONMENT}.corp:${INFLUXDB_PORT}"
+fi
+
+echo $INFLUXDB_HOST
+
+curl "http://${GRAFANA_USERNAME}:${GRAFANA_PASSWORD}@${GRAFANA_HOST}:${GRAFANA_PORT}/api/datasources" \
     -X POST -H "Content-Type: application/json" \
     --data-binary <<DATASOURCE \
       '{
         "name":"influx",
         "type":"influxdb",
-        "url":"http://localhost:8086",
+        "url":"'"${INFLUXDB_API}"'",
         "access":"direct",
         "isDefault":true,
         "database":"services",
